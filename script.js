3b8be833f4b66b3a5a7a353e4576663aa71ccec7
Gaura-krishna <118505849+Gaura-krishna@users.noreply.github.com>
2023-02-24 18:24:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Data Science", "Full Stack Development", "Amazon Web Service", "Digital Marketing"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+    
+})
